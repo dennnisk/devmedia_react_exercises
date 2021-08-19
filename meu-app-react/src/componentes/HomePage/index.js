5530fa016f865b3e590751d0cfb11d00e45399c2
Denis <dennnis.projects@gmail.com>
2021-08-19 20:52:02 -0300

    Teste de Rotas com React

--- meu-app-react/src/componentes/HomePage/index.js
+++ meu-app-react/src/componentes/HomePage/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Card from '../Card/';
+import Contador from '../Contador/';
 import './style.css';
 
 export default function HomePage() {
   return (
-    <div className="home-page">
-      <Card />
-      <Card />
-      <Card />
+    <div>
+      <div className="home-page">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <Contador />
     </div>
   );
 }
